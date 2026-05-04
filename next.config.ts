953d0a48442967083c7a4b04953482f3cf006e5b
Z User <z@container>
2026-05-04 05:59:48 +0000

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+  output: "export",
+  basePath: "/quran",
+  assetPrefix: "/quran/",
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
